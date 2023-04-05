2a7a6dea7f94b41788548c52eebe712fbaf4195c
Haruki Kondo <avp.104.106.107@ae.auone-net.jp>
2023-04-05 09:42:25 +0000

    サンプル用のコードを作成

---
+++ backend/migrations/1_deploy.js
@@ -0,0 +1,7 @@
+const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+
+const LogicV1 = artifacts.require('LogicV1');
+
+module.exports = async function (deployer) {
+  const instance = await deployProxy(LogicV1, [42], { deployer });
+}
